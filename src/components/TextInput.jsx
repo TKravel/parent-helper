982dcb62b14425e7d77f9d4244e8bf7eb56fbfff
Tim Kravel <TKravel@Yahoo.com>
2021-07-29 23:05:43 -0400

    text input validation

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TextInput(props){
+    const [error, setError] = useState({
+        error: false,
+        message: "Input required"
+    });
+
+    function validateInput(e){
+        if(props.stateData === ""){
+            setError(prevValue => {
+                return({
+                    error: true,
+                    message: prevValue.message
+                })
+            })
+            e.preventDefault();
+        } else {
+            setError(prevValue => {
+                return({
+                    error: false,
+                    message: prevValue.message
+                })
+            })
+            props.updateState(e);
+            e.preventDefault();
+        }
+        
+    }
+
     return(
         <form>
             <label htmlFor={props.name}>{props.label}</label>
             <input type="text" name={props.name} onChange={props.updateChange} value={props.stateData} placeholder={props.placeholder} />
-            <button onClick={props.updateState}>Add</button>
-          </form>
+            <button onClick={validateInput}>Add</button>
+            { error.error === !false ? <p>{error.message}</p> : null }
+        </form>
     )
 }
 
